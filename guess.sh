707b7e7c61c517ee1f47f728040f4968efb55da9
Claire_Neumann <neumancl@mail.gvsu.edu>
2025-10-27 15:26:29 -0400

    modified for loop with seq

--- guess.sh
+++ guess.sh
@@ -3,14 +3,16 @@
 # This script gives the user five tries to guess a secret number 1-10 (inclusive). 
 #/.guess.sh <GUESS_NUMBER_1-10>
 
-
+tries=5
 secretnum=$(( (RANDOM % 10) + 1 ))
 guess=$1
 
 
-echo $secretnum
-echo $guess
 
-
-for num in {1..5}; do
+for num in $(seq 1 $tries); do
         read -p "Guess the secret number:" guess
+
+	if ![["$guess" =~ [1-10]]]; then
+		echo "Please enter a number 1 through 10"
+		continue
+	
