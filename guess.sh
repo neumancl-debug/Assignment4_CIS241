b8b08952881252761cbed7cee396a5e85b53ea35
Claire_Neumann <neumancl@mail.gvsu.edu>
2025-10-26 21:51:44 -0400

    set up file and added for loop

--- guess.sh
+++ guess.sh
@@ -1,1 +1,16 @@
-!#/bin/bash
+#!/bin/bash
+
+# This script gives the user five tries to guess a secret number 1-10 (inclusive). 
+#/.guess.sh <GUESS_NUMBER_1-10>
+
+
+secretnum=$(( (RANDOM % 10) + 1 ))
+guess=$1
+
+
+echo $secretnum
+echo $guess
+
+
+for num in {1..5}; do
+        read -p "Guess the secret number:" guess
