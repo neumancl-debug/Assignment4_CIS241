cbfa56c0f1ade47c0265def890b8959b7384c964
Claire_Neumann <neumancl@mail.gvsu.edu>
2025-10-27 20:45:09 -0400

    completed file with input validation

--- guess.sh
+++ guess.sh
@@ -5,21 +5,31 @@
 
 tries=5
 secretnum=$(( (RANDOM % 10) + 1 ))
-guess=$1
-
-
+attempts=1
 
 for num in $(seq 1 $tries); do
-        read -p "Guess the secret number (1-10):" guess
+        
+	if (( num ==1 )) && [[ $1 ]]; then
+		guess=$1
+	else
+		read -p "Guess the secret number (1-10):" guess
+	fi
+
+	if ! [[ "$guess" =~ ^[0-9]+$ ]] || (( guess < 1 || guess > 10 )); then
+        	echo "Please enter a valid number between 1 and 10."
+        	continue
+    	fi
+
 
         if ((guess == secretnum)); then
-                echo "Congrats! You guessed the secret number!"
+                echo "Congrats! You guessed the secret number! You used $attempts attempts!"
                 exit 0
         elif ((guess < secretnum)); then
                 echo "your guess is too low..."
         else
                 echo "your guess is too high.."
         fi
+	attempts=$((attempts + 1))
 done
 
-echo "You've used all your tries! The secret number was $secretnum."
+echo "You've used all your attempts! The secret number was $secretnum."
