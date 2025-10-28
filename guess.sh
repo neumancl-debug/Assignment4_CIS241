86511be29faa5a5bbd80ba2ed28424ff75abd068
Claire_Neumann <neumancl@mail.gvsu.edu>
2025-10-27 20:30:52 -0400

    made funtional

--- guess.sh
+++ guess.sh
@@ -10,21 +10,16 @@ guess=$1
 
 
 for num in $(seq 1 $tries); do
-        read -p "Guess the secret number:" guess
+        read -p "Guess the secret number (1-10):" guess
 
-	if ![["$guess" =~ [1-10]]]; then
-		echo "Please enter a number 1 through 10"
-		continue
-	fi
-
-	if (($guess == $secretnum)); then
-		echo "Congrats! You guessed the secret number!
-		exit
-	elif (($guess < $secretnum)); then
-		echo "your guess is too low..."
-	else
-		echo "your guess is too high.."
-	fi
+        if ((guess == secretnum)); then
+                echo "Congrats! You guessed the secret number!"
+                exit 0
+        elif ((guess < secretnum)); then
+                echo "your guess is too low..."
+        else
+                echo "your guess is too high.."
+        fi
 done
 
-echo "You\'ve used all your tries! The secret number was $secretnum."
+echo "You've used all your tries! The secret number was $secretnum."
